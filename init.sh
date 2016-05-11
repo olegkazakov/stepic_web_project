56fe5c788469c9d2954951c3d245b59eddb01d80
olegkazakov <olegkazakov31@gmail.com>
2016-05-12 01:26:56 +0300

    Hello application added, gunicorn and nginx configured

--- init.sh
+++ init.sh
@@ -1,2 +1,12 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+#nginx conf
+if [ -f /etc/nginx/sites-enabled/default ]; then
+    sudo rm /etc/nginx/sites-enabled/default
+fi
+
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+
+#gunicorn conf
+touch /home/box/gunicorn.log
+sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
