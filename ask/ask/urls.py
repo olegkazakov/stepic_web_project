3691f165cc236b21702bbc915617bb66c8fdc1c0
olegkazakov <olegkazakov31@gmail.com>
2016-05-14 16:12:29 +0300

    Config urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', include('qa.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
