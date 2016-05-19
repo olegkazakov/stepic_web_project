1381e2cb62cc202b43a421aabbb16db20646ab93
olegkazakov <olegkazakov31@gmail.com>
2016-05-20 01:16:33 +0300

    Config 80 port

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ if [ -f /etc/nginx/sites-enabled/default ]; then
     sudo rm /etc/nginx/sites-enabled/default
 fi
 
+touch /home/box/nginx.log
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
@@ -12,8 +13,8 @@ sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
 
-mysql -uroot -e "CREATE DATABASE ask CHARACTER SET utf8 COLLATE utf8_general_ci"
-mysql -uroot -e "CREATE USER 'ok_ask'@'localhost' IDENTIFIED BY 'ok_ask'"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON ask.* TO 'ok_ask'@'localhost'"
+#mysql -uroot -e "CREATE DATABASE ask CHARACTER SET utf8 COLLATE utf8_general_ci"
+#mysql -uroot -e "CREATE USER 'ok_ask'@'localhost' IDENTIFIED BY 'ok_ask'"
+#mysql -uroot -e "GRANT ALL PRIVILEGES ON ask.* TO 'ok_ask'@'localhost'"
 
 python /home/box/web/ask/manage.py syncdb
