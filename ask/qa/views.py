59674a19bc4a785e79f4be1708db1e93fa0c0abf
olegkazakov <olegkazakov31@gmail.com>
2016-05-20 00:02:35 +0300

    Questions list, popular questions and detail view added

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,7 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponse
+from django.core.urlresolvers import reverse
+from django.core.paginator import Paginator, EmptyPage
+from qa.models import Question
 
-# Create your views here.
-
-from django.http import HttpResponse
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
+def paginate(request, questions):
+    try:
+        limit = int(request.GET.get('limit', 3))
+    except ValueError:
+        limit = 3
+    if limit > 100:
+        limit = 3
+
+    try:
+        page = int(request.GET.get('page', 1))
+    except ValueError:
+        raise Http404
+
+    paginator = Paginator(questions, limit)
+
+    try:
+        page = paginator.page(page)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+    return page, paginator
+
+def list(request):
+    questions = Question.objects.all()
+    questions = questions.order_by('-added_at')
+    page, paginator = paginate(request, questions)
+    paginator.baseurl = reverse('list') + '?page='
+
+    return render(request, 'list.html', {
+        'questions': page.object_list,
+        'page': page,
+        'paginator': paginator,
+    })
+
+def popular(request):
+    questions = Question.objects.all()
+    questions = questions.order_by('-rating')
+    page, paginator = paginate(request, questions)
+    paginator.baseurl = reverse('popular') + '?page='
+
+    return render(request, 'list_popular.html', {
+        'questions': page.object_list,
+        'page': page,
+        'paginator': paginator,
+    })
+
+def question_detail(request, pk):
+    question = get_object_or_404(Question, id=pk)
+    answers = question.answer_set.all()
+
+    return render(request, 'detail.html', {
+        'question': question,
+        'answers': answers,
+    })
