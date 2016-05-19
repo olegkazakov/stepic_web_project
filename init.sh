f894b99b0e1dbbe518b72b0e5d1e5aa2bfa82954
olegkazakov <olegkazakov31@gmail.com>
2016-05-20 01:21:48 +0300

    Mysql database connetion reversed

--- init.sh
+++ init.sh
@@ -13,8 +13,8 @@ sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
 
-#mysql -uroot -e "CREATE DATABASE ask CHARACTER SET utf8 COLLATE utf8_general_ci"
-#mysql -uroot -e "CREATE USER 'ok_ask'@'localhost' IDENTIFIED BY 'ok_ask'"
-#mysql -uroot -e "GRANT ALL PRIVILEGES ON ask.* TO 'ok_ask'@'localhost'"
+mysql -uroot -e "CREATE DATABASE ask CHARACTER SET utf8 COLLATE utf8_general_ci"
+mysql -uroot -e "CREATE USER 'ok_ask'@'localhost' IDENTIFIED BY 'ok_ask'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON ask.* TO 'ok_ask'@'localhost'"
 
 python /home/box/web/ask/manage.py syncdb
