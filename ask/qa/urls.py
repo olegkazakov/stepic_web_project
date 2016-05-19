6a2bbd104e514c1e248dbc60a734557835365fa2
olegkazakov <olegkazakov31@gmail.com>
2016-05-20 00:09:50 +0300

    Configured views and urls

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url
-from qa.views import test
+from django.conf import settings
+from django.conf.urls.static import static
+from qa.views import test, list, popular, question_detail
 
 urlpatterns = [
-    url(r'^$', test, name='home'),
+    url(r'^$', list, name='list'),
+    url(r'^question/(?P<pk>\d+)/', question_detail, name='question_detail'),
     url(r'^login/', test, name='login'),
     url(r'^signup/', test, name='signup'),
-    url(r'^question/\d+/.*$', test, name='question'),
     url(r'^ask/', test, name='ask'),
-    url(r'^popular/', test, name='popular'),
+    url(r'^popular/', popular, name='popular'),
     url(r'^new/', test, name='new')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
